cb0fbf20a850d6d521c5cc85fc37f92ee95d238f
arturito <58474213+arthures11@users.noreply.github.com>
2023-08-27 14:44:05 +0200

    v1.0

--- src/main/java/com/bryja/pluginhej/MyToolWindowFactory.java
+++ src/main/java/com/bryja/pluginhej/MyToolWindowFactory.java
@@ -1,11 +1,18 @@
 package com.bryja.pluginhej;
 
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileAdapter;
+import com.intellij.openapi.vfs.VirtualFileEvent;
+import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowContentUiType;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import com.sun.istack.NotNull;
+
 import javax.swing.*;
 
 public class MyToolWindowFactory implements ToolWindowFactory {
@@ -17,5 +24,64 @@ public class MyToolWindowFactory implements ToolWindowFactory {
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(panel, "", false);
         toolWindow.getContentManager().addContent(content);
+
+        JList<String> fileList = new JList<>(listProjectFiles(project));
+        fileList.addListSelectionListener(e -> openSelectedFile(project, fileList.getSelectedValue()));
+        VirtualFileManager.getInstance().addVirtualFileListener(new MyFileChangeListener(fileList, project));
+        panel.add(new JScrollPane(fileList));
     }
+
+    private String[] listProjectFiles(Project project) {
+        // Retrieve a list of project files (VirtualFiles) and convert them to strings.
+        VirtualFile[] files = project.getBaseDir().getChildren();
+        String[] fileNames = new String[files.length];
+        for (int i = 0; i < files.length; i++) {
+            fileNames[i] = files[i].getName();
+        }
+        return fileNames;
+    }
+
+    private void openSelectedFile(Project project, String fileName) {
+        // Open the selected file in the editor.
+        VirtualFile file = project.getBaseDir().findChild(fileName);
+        if (file != null) {
+            FileEditorManager.getInstance(project).openFile(file, true);
+        }
+    }
+
+
+}
+
+class MyFileChangeListener extends VirtualFileAdapter {
+    private final JList<String> fileList;
+
+    private final Project project;
+    public MyFileChangeListener(JList<String> fileList, Project project) {
+        this.fileList = fileList;
+        this.project=project;
+    }
+
+    @Override
+    public void fileCreated(@NotNull VirtualFileEvent event) {
+        // Refresh the list when a file is created.
+        fileList.setListData(listProjectFiles(project));
+    }
+
+    @Override
+    public void fileDeleted(@NotNull VirtualFileEvent event) {
+        // Refresh the list when a file is deleted.
+        fileList.setListData(listProjectFiles(project));
+    }
+
+    private String[] listProjectFiles(Project project) {
+        // Retrieve a list of project files (VirtualFiles) and convert them to strings.
+        VirtualFile[] files = project.getBaseDir().getChildren();
+        String[] fileNames = new String[files.length];
+        for (int i = 0; i < files.length; i++) {
+            fileNames[i] = files[i].getName();
+        }
+        return fileNames;
+    }
+
+
 }
